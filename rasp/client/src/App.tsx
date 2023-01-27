9f424ceeeae956e3e964316bac3ceb8bbae32c61
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-27 20:18:01 +0530

    added the code to fetch requstData and on change send the get request the server to reflect the same

--- rasp/client/src/App.tsx
+++ rasp/client/src/App.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
+import axios from "axios";
 
 function App() {
+    const BACKEND_URI = process.env.REACT_APP_BACKEND_URI;
+
+    const [requestDataToogle, setRequestDataToogle] = useState<boolean | null>(
+        null
+    );
+
+    useEffect(() => {
+        console.log("Fetching the current status of request data.");
+        axios({ method: "GET", url: `${BACKEND_URI}/getRequestData` }).then(
+            (resp) => {
+                setRequestDataToogle(resp.data);
+            }
+        );
+    }, [BACKEND_URI]);
+
+    useEffect(() => {
+        if (requestDataToogle === true) {
+            axios({
+                method: "GET",
+                url: `${BACKEND_URI}/startRequestData`,
+            }).then((resp) => {
+                console.log(resp.data);
+            });
+        } else if (requestDataToogle === false) {
+            axios({
+                method: "GET",
+                url: `${BACKEND_URI}/stopRequestData`,
+            }).then((resp) => {
+                console.log(resp.data);
+            });
+        }
+    }, [requestDataToogle, BACKEND_URI]);
+
     return <div className="App">Hello World!</div>;
 }
 
