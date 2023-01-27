ea03db6f317fceb5d9a063544175999b38270f5a
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-27 19:21:58 +0530

    requestData api calls

--- rasp/server/app.js
+++ rasp/server/app.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
+
+let requestData = false;
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Ok" });
+});
+
+app.get("/getRequestData", (req, res) => {
+    res.send(requestData);
+});
+
+app.get("/stopRequestData", (req, res) => {
+    requestData = false;
+    res.status(200).json({ message: "Done." });
+});
+
+app.get("/startRequestData", (req, res) => {
+    requestData = true;
+    res.status(200).json({ message: "Done" });
 });
 
 module.exports = app;
